983c6d519edde79f23c9ccd6daa721ada1cb01e4
Monica Washington <monica.lynnwashington@gmail.com>
2026-08-28 16:03:42 -0600

    Completed Weekly Challenges Set 5

--- ChallengesWithTestsMark8.Tests/obj/Debug/net9.0/ChallengesWithTestsMark8.Tests.AssemblyInfo.cs
+++ ChallengesWithTestsMark8.Tests/obj/Debug/net9.0/ChallengesWithTestsMark8.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChallengesWithTestsMark8.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b83bf016970504e6710626f757b16e5a1dd2a84a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ba2eec1f0625e0a58d0f3855142b336778cdb11")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChallengesWithTestsMark8.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChallengesWithTestsMark8.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
